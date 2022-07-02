8f8fa4cc5ac40a6ce9a161cffc4cddd1b6dd5646
chrisforee <chrisforee@gmail.com>
2022-07-02 09:09:25 -0500

    first commit

---
+++ algos.js
@@ -0,0 +1,42 @@
+// Algos prcatice
+
+// Level 1 
+
+console.log("Algo 1");
+var x = 5;
+console.log(x);
+console.log("End of Algo 1")
+
+var x = 3;
+x = x+1;
+console.log(x);
+
+var x = 3;
+x = x+2;
+console.log(x+2);
+console.log(x);
+
+var x = 3;
+x = x*x;
+console.log(x);
+console.log(x*2);
+
+var x = 2;
+var y = 5;
+var z = x+y;
+console.log(z);
+
+
+// Level 2
+
+for(var i=1; i<100; i++)
+{
+    if(i == 10)
+    {
+        break;
+    }
+    console.log(i)
+}
+
+var x = [['coding', 'boom'], ['dojo', 'yeah']];
+console.log(x[0][0] + '-' + x[1][0]);
