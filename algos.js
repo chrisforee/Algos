683d56d971e2043cf35d2d42e176d65629b61b10
chrisforee <chrisforee@gmail.com>
2022-07-03 07:42:09 -0500

    added more algos

--- algos.js
+++ algos.js
@@ -2,10 +2,9 @@
 
 // Level 1 
 
-console.log("Algo 1");
+
 var x = 5;
 console.log(x);
-console.log("End of Algo 1")
 
 var x = 3;
 x = x+1;
@@ -28,6 +27,55 @@ console.log(z);
 
 
 // Level 2
+
+var x = [1,2,4];
+console.log(x);
+
+var x = [1,3,5];
+console.log(x[0]);
+
+var x = [2,4,6];
+console.log(x[1]);
+
+var x = [1,3,5];
+console.log(x[2]);
+
+var c = [1.3];
+console.log(x[0]);
+
+var x = [1,2,4];
+x[0] = x[1];
+console.log(x);
+
+var x = [1,3,5];
+var y = x.length;
+console.log(y);
+
+var x = [2,4,6,3,7];
+var y = x.length -2;
+console.log(x[y]);
+
+var x = [2,4,6,3,7];
+var y = x.length;
+console.log(x[y-1]);
+
+var x = [1,3,5,7];
+x[0] = x[x.length-2];
+console.log(x);
+
+var x = [1,3,5,7];
+x[x.length-3] = x[x.length/2+x.length/4];
+console.log(x);
+
+var x = [1,3,5,7];
+var y = [2,4,6];
+x[2] = y[x.length-3];
+console.log(x);
+
+var x = [1,3,5,7];
+var y = [2,4,6];
+x[2] = y[x.length-3] + x[x[0] + y[0]];
+console.log(x);
 
 for(var i=1; i<100; i++)
 {
